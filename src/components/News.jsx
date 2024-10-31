c538bb0edffc7f1fa89c93e70f6388e676ccf0a7
tubi55 <tubi55@nate.com>
2024-10-31 13:23:27 +0900

    scroll에 시간차 적용

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+
 export default function News({ Scroll, pos }) {
 	const currentScroll = Scroll - pos || 0;
+	const Data = ['text1', 'text2', 'text'];
 
 	const style_h2 = {
 		left: currentScroll * 6
@@ -8,18 +11,18 @@ export default function News({ Scroll, pos }) {
 		transform: `translateX(${currentScroll * 1.5}px)`
 	};
 
-	/*
-	미션 (1시 17분까지) - 아래 article항목이 서로 다른 속도로 아래로 내려가도록 scroll값 연동
-	*/
-
 	return (
 		<section className='news'>
 			<h2 style={style_h2}>POST</h2>
 			<h3 style={style_h3}>INFORMATION</h3>
 
-			<article></article>
-			<article></article>
-			<article></article>
+			{Data.map((el, idx) => {
+				return (
+					<article style={{ transform: `translateY(${currentScroll * 0.3 * idx}px)` }} key={idx}>
+						{el}
+					</article>
+				);
+			})}
 		</section>
 	);
 }

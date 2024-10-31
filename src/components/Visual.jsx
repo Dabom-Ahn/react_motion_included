95b45e7bcbc64f44f708e649f99c076b74708429
tubi55 <tubi55@nate.com>
2024-10-31 11:15:55 +0900

    3d효과 적용

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,15 +1,18 @@
-/*
-	미션 - 제목요소가 확대되면서 완전 투명해지도록 스크롤값 연동
-*/
-
 export default function Visual({ Scroll }) {
 	const style_h2 = {
-		transform: `translateX(${Scroll}px) translateY(${Scroll}px) rotate(${Scroll * 1.5}deg)`
+		transform: `translateX(${Scroll}px) translateY(${Scroll}px) rotate(${Scroll * 1.5}deg) scale(${1 + Scroll / 400})`,
+		opacity: 1 - Scroll / 400
+	};
+
+	const style_box1 = {
+		transform: `rotateY(${Scroll}deg)`,
+		opacity: 1 - Scroll / 800
 	};
 
 	return (
 		<section className='visual'>
 			<h2 style={style_h2}>ALPACO</h2>
+			<div style={style_box1} className='box1'></div>
 		</section>
 	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Visual from '../components/Visual';
 
 export default function Home() {
 	const [Scroll, setScroll] = useState(0);
-	console.log(Scroll);
 
 	const handleScroll = () => {
 		setScroll(window.scrollY);
@@ -18,12 +17,6 @@ export default function Home() {
 			window.removeEventListener('scroll', handleScroll);
 		};
 	}, []);
-
-	//윈도우 스크롤시 현재 스크롤되는 거리값을 Scroll상태값에 담아주는 함수 생성
-	//해당 함수를 윈도우 스크롤이벤트 연결
-	//컴포넌트 언마운트시 스크롤이벤트 해제
-	//비주얼 컴포넌트에 스크롤값 전달
-	//Visual안쪽에서 전달되는 Scroll값을 h2요소 원하는 스타일 구문에 연동
 
 	return (
 		<>

5ba5ae76b7ce0b82a02c39e149d19b65c958cd8b
tubi55 <tubi55@nate.com>
2024-10-31 10:24:15 +0900

    스크롤 기본 모션 연동 및 이슈사항

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,3 +1,12 @@
-export default function Visual() {
-	return <section className='visual'>Viusal</section>;
+export default function Visual({ Scroll }) {
+	const style_h2 = {
+		left: Scroll,
+		top: Scroll
+	};
+
+	return (
+		<section className='visual'>
+			<h2 style={style_h2}>ALPACO</h2>
+		</section>
+	);
 }

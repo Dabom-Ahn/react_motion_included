534a0287245a5698bf5356d4f796caadd65f26d3
tubi55 <tubi55@nate.com>
2024-10-31 10:42:08 +0900

    현재 위치값을 고수하면서 스크롤 모션 적용

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,7 +1,10 @@
+/*
+	미션 - 제목요소가 확대되면서 완전 투명해지도록 스크롤값 연동
+*/
+
 export default function Visual({ Scroll }) {
 	const style_h2 = {
-		left: Scroll,
-		top: Scroll
+		transform: `translateX(${Scroll}px) translateY(${Scroll}px) rotate(${Scroll * 1.5}deg)`
 	};
 
 	return (
